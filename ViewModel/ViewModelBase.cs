f4faaa6b6b73be88f5c5d9d280c1f12c56447e27
parkjunyoung <hpaaycim1@naver.com>
2021-12-24 01:32:17 +0900

    202112240132

---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meiday
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        //View와 바인딩된 속성값이 바뀔때 이를 WPF에게 알리기 위한 이벤트
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
