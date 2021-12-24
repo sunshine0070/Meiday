a47c74cb59f4d5266c1713ac696f9f3bea6d18eb
parkjunyoung <hpaaycim1@naver.com>
2021-12-24 13:17:55 +0900

    first setting

---
+++ TelerikWpfApp1/Model/DataSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelerikWpfApp1.Model
+{
+    public class ment
+    {
+		public string regNum { get; set; }
+		public int iDNum { get; set; }
+		public string patientName { get; set; }
+		public bool IsChecked02 { get; set; }
+		public string InsuName { get; set; }
+		public string InsuProduct { get; set; }
+	}
+}
