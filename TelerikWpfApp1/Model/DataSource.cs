e49f2a29a882dc6fb37aa3ab28d8d7c0c744556e
parkjunyoung <hpaaycim1@naver.com>
2021-12-29 17:02:06 +0900

    first_all

---
+++ TelerikWpfApp1/Model/DataSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meiday.Model
+{
+    public class ment
+    {
+		public string regNum { get; set; }
+		public int iDNum { get; set; }
+		public string patientName { get; set; }
+		public bool IsChecked02 { get; set; }
+		public string InsuName { get; set; }
+		public string InsuProduct { get; set; }
+	}
+
+	public class payment
+	{
+		public string Name { get; set; }
+		public string Doctor { get; set; }
+		public string Group { get; set; }
+		public string Date { get; set; }
+		public string Price { get; set; }
+		public bool Checked { get; set; }
+	}
+}
