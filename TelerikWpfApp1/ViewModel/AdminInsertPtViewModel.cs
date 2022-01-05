84fe65f26699f4715086b57028dfda3236645b8b
sylviethebest <88299347+sylviethebest@users.noreply.github.com>
2022-01-05 16:59:26 +0900

    관리자페이지 환자입력 코드 수정
    textbox가 빈 경우 입력 안됨, messagebox로 필요 조건에 대해 명시.
    나이, 전화번호, 등록번호, 주민등록번호 숫자 입력하지 않을 시 팝업창 뜸..

--- TelerikWpfApp1/ViewModel/AdminInsertPtViewModel.cs
+++ TelerikWpfApp1/ViewModel/AdminInsertPtViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Windows;
 using Meiday.Model;
 
 namespace Meiday.ViewModel
@@ -143,52 +144,89 @@ namespace Meiday.ViewModel
                 Phone = this.phone,
                 Addr = this.addr,
             };
-
-            if (this.idNum == null)
+            if (this.name != null && this.age != null && this.regNum != null && this.phone != null && this.addr != null)
             {
-                string query = @"MERGE INTO PATIENT USING dual ON (PT_IDNUM = '#IdNum') 
+                if (this.idNum == null)
+                {
+                    string query = @"MERGE INTO PATIENT USING dual ON (PT_IDNUM = '#IdNum') 
                             WHEN MATCHED THEN UPDATE SET PT_NAME = '#Name', PT_ADDR = '#Addr', PT_PHONE = '#Phone' ,  PT_AGE = '#Age' , PT_REGNUM = '#RegNum' 
                             WHEN NOT MATCHED THEN INSERT (PT_REGNUM,PT_NAME,PT_ADDR,PT_PHONE,PT_AGE) VALUES ('#RegNum','#Name', '#Addr', '#Phone', '#Age') ";
-                string query1 = @"commit";
-                query = query.Replace("#IdNum", this.idNum);
-                query = query.Replace("#Name", this.name);
-                query = query.Replace("#Age", this.age);
-                query = query.Replace("#RegNum", this.regNum);
-                query = query.Replace("#Phone", this.phone);
-                query = query.Replace("#Addr", this.addr);
-                OracleDBManager.Instance.ExecuteNonQuery(query);
-                OracleDBManager.Instance.ExecuteNonQuery(query1);
+                    string query1 = @"commit";
+                    query = query.Replace("#IdNum", this.idNum);
+                    query = query.Replace("#Name", this.name);
+                    query = query.Replace("#Age", this.age);
+                    query = query.Replace("#RegNum", this.regNum);
+                    query = query.Replace("#Phone", this.phone);
+                    query = query.Replace("#Addr", this.addr);
+                    OracleDBManager.Instance.ExecuteNonQuery(query);
+                    OracleDBManager.Instance.ExecuteNonQuery(query1);
 
-                this.idNum = string.Empty;
-                this.name = string.Empty;
-                this.age = string.Empty;
-                this.regNum = string.Empty;
-                this.addr = string.Empty;
-                this.phone = string.Empty;
-            }
-            else 
-            {
-                string query = @"MERGE INTO PATIENT USING dual ON (PT_IDNUM = '#IdNum') 
+                    this.idNum = string.Empty;
+                    this.name = string.Empty;
+                    this.age = string.Empty;
+                    this.regNum = string.Empty;
+                    this.addr = string.Empty;
+                    this.phone = string.Empty;
+                }
+                else
+                {
+                    string query = @"MERGE INTO PATIENT USING dual ON (PT_IDNUM = '#IdNum') 
                             WHEN MATCHED THEN UPDATE SET PT_NAME = '#Name', PT_ADDR = '#Addr', PT_PHONE = '#Phone' ,  PT_AGE = '#Age' , PT_REGNUM = '#RegNum' 
                             WHEN NOT MATCHED THEN INSERT (PT_IDNUM,PT_REGNUM,PT_NAME,PT_ADDR,PT_PHONE,PT_AGE) VALUES ('#IdNum','#RegNum','#Name', '#Addr', '#Phone', '#Age') ";
-                string query1 = @"commit";
-                query = query.Replace("#IdNum", this.idNum);
-                query = query.Replace("#Name", this.name);
-                query = query.Replace("#Age", this.age);
-                query = query.Replace("#RegNum", this.regNum);
-                query = query.Replace("#Phone", this.phone);
-                query = query.Replace("#Addr", this.addr);
-                OracleDBManager.Instance.ExecuteNonQuery(query);
-                OracleDBManager.Instance.ExecuteNonQuery(query1);
+                    string query1 = @"commit";
+                    query = query.Replace("#IdNum", this.idNum);
+                    query = query.Replace("#Name", this.name);
+                    query = query.Replace("#Age", this.age);
+                    query = query.Replace("#RegNum", this.regNum);
+                    query = query.Replace("#Phone", this.phone);
+                    query = query.Replace("#Addr", this.addr);
+                    OracleDBManager.Instance.ExecuteNonQuery(query);
+                    OracleDBManager.Instance.ExecuteNonQuery(query1);
 
-                this.idNum = string.Empty;
-                this.name = string.Empty;
-                this.age = string.Empty;
-                this.regNum = string.Empty;
-                this.addr = string.Empty;
-                this.phone = string.Empty;
+                    this.idNum = string.Empty;
+                    this.name = string.Empty;
+                    this.age = string.Empty;
+                    this.regNum = string.Empty;
+                    this.addr = string.Empty;
+                    this.phone = string.Empty;
+                }
+            }
+            else
+            {
+                // textbox가 비었을 때 미충족 항목 MessageBox 팝업 띄워서 표시하기
+                string result = "";
+                string last = " 을(를) 입력해주세요.";
+                if (this.name != string.Empty) { result += " 이름,"; }
+                if (this.age != string.Empty) { result += " 나이,"; }
+                if (this.regNum != string.Empty) { result += " 주민등록번호,"; }
+                if (this.phone != string.Empty) { result += " 전화번호,"; }
+                if (this.addr != string.Empty) { result += " 주소,"; }
+                if (result.Contains(','.ToString()))
+                {
+                    result = result.Remove(result.LastIndexOf(','.ToString()));
+                }
+                if (result != null)
+                {
+                    MessageBox.Show(result + last);
+                }
+
+            }
+            if(!this.idNum.StartsWith("22"))
+            {
+                MessageBox.Show("등록번호 형식에 맞게 입력해주세요.\n ex)2022년도 입원 : 22###");
+            }
+            if(this.idNum.IsNumeric() || this.age.IsNumeric() || this.regNum.IsNumeric() || this.phone.IsNumeric())
+            {
+                MessageBox.Show("정보를 정확하게 입력해주세요.");
             }
         }
-
+    }
+    public static class StringExtensions
+    {
+        public static bool IsNumeric(this string input)
+        {
+            int number;
+            return int.TryParse(input, out number);
+        }
     }
 }
