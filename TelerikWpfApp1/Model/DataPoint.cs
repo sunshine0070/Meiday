60c332ad6d5dbe4f6b220f24f2c04cdb8cd9bb7d
parkjunyoung <hpaaycim1@naver.com>
2022-01-09 15:33:22 +0900

    전체_수정

--- TelerikWpfApp1/Model/DataPoint.cs
+++ TelerikWpfApp1/Model/DataPoint.cs
@@ -8,7 +8,22 @@ namespace Meiday.Model
 {
     public class DataPoint
     {
+<<<<<<< HEAD
+        public string str_date { get; set; } // 요일
+
+        public string pharmacy_name { get; set; } // 약국 이름
+        public string insurance_name { get; set; } // 약국 이름
+
+        public int Value { get; set; } // 값
+=======
         public string str_date { get; set; }
         public int Value { get; set; }
+        public string dep_name { get; set; }
+        public string dep_Ages { get; set; }
+        public int dep_count { get; set; }
+<<<<<<< Updated upstream
+=======
+>>>>>>> 2e9edc638300b8504f480f94c23931b942958457
+>>>>>>> Stashed changes
     }
 }
---
+++ TelerikWpfApp1/View/Admin_Dep_per_Ages.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Meiday.View
+{
+    /// <summary>
+    /// Admin_Dep_per_Ages.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Admin_Dep_per_Ages : UserControl
+    {
+        public Admin_Dep_per_Ages()
+        {
+            InitializeComponent();
+        }
+    }
+}
