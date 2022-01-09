7bf5020799e7e1c413d6ac9ab60bf8d184cc14c7
parkjunyoung <hpaaycim1@naver.com>
2022-01-09 16:24:39 +0900

    data_point.cs 수정

--- TelerikWpfApp1/Model/DataPoint.cs
+++ TelerikWpfApp1/Model/DataPoint.cs
@@ -9,6 +9,11 @@ namespace Meiday.Model
     public class DataPoint
     {
         public string str_date { get; set; }
+        public string pharmacy_name { get; set; } // 약국 이름
+        public string insurance_name { get; set; } // 약국 이름
         public int Value { get; set; }
+        public string dep_name { get; set; }
+        public string dep_Ages { get; set; }
+        public int dep_count { get; set; }
     }
 }
