ca5a0bebda9b6cf9ce2763a0a885cdc4416d3920
Eol Kim <32378990+KimEul@users.noreply.github.com>
2022-01-09 14:34:41 +0900

    머지 ?

--- TelerikWpfApp1/Model/DataPoint.cs
+++ TelerikWpfApp1/Model/DataPoint.cs
@@ -10,5 +10,8 @@ namespace Meiday.Model
     {
         public string str_date { get; set; }
         public int Value { get; set; }
+        public string dep_name { get; set; }
+        public string dep_Ages { get; set; }
+        public int dep_count { get; set; }
     }
 }
