2921bd51322f531933ffa9292e9d117a79dc5bfc
Eol Kim <32378990+KimEul@users.noreply.github.com>
2022-01-09 16:49:48 +0900

    Update DataPoint.cs
    test1234

--- TelerikWpfApp1/Model/DataPoint.cs
+++ TelerikWpfApp1/Model/DataPoint.cs
@@ -13,6 +13,9 @@ namespace Meiday.Model
         public string insurance_name { get; set; } // 약국 이름
         public int Value { get; set; }
         public string dep_name { get; set; }
+
+        //test1234
+        
         public string dep_Ages { get; set; }
         public int dep_count { get; set; }
     }
