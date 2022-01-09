ccc88f1f3fa0160e2a474b802e5feb94703fc864
parkjunyoung <hpaaycim1@naver.com>
2022-01-09 15:12:43 +0900

    Revert "Merge branch 'main' of https://github.com/parkjunyoung27/Meiday"
    This reverts commit 2e9edc638300b8504f480f94c23931b942958457, reversing
    changes made to d9338e519e9f22d7e8d13a91601bd28e0e332462.

--- TelerikWpfApp1/Model/DataPoint.cs
+++ TelerikWpfApp1/Model/DataPoint.cs
@@ -10,8 +10,5 @@ namespace Meiday.Model
     {
         public string str_date { get; set; }
         public int Value { get; set; }
-        public string dep_name { get; set; }
-        public string dep_Ages { get; set; }
-        public int dep_count { get; set; }
     }
 }
