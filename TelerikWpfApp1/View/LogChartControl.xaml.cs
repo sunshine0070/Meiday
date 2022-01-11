5b026311c6441b4a1f05a14bb0149a8bbdb84722
sylviethebest <88299347+sylviethebest@users.noreply.github.com>
2022-01-11 18:26:50 +0900

    실시간로그차트(수정필요)

---
+++ TelerikWpfApp1/View/LogChartControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Xpf.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Meiday.View
+{
+    /// <summary>
+    /// LogChartControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LogChartControl : UserControl
+    {
+        public LogChartControl()
+        {
+            InitializeComponent();
+        }
+        private void chart_BoundDataChanged(object sender, RoutedEventArgs e)
+        {
+            // Adjust the visual range.
+            AxisX2D axisX = ((XYDiagram2D)chart.Diagram).ActualAxisX;
+            DateTime maxRangeValue = (DateTime)axisX.ActualWholeRange.ActualMaxValue;
+            axisX.ActualVisualRange.SetMinMaxValues(maxRangeValue.AddSeconds(-10), maxRangeValue);
+        }
+    }
+}
---
+++ TelerikWpfApp1/ViewModel/LogChartViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Meiday.ViewModel
+{
+    public class LogChartViewModel
+    {
+        const int MaxPointCount = 3000;
+        readonly DispatcherTimer timer = new DispatcherTimer();
+        public ObservableCollection<LogPoint> DataPoints { get; } = new ObservableCollection<LogPoint>();
+        public LogChartViewModel()
+        {
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, System.EventArgs e)  {
+            this.DataPoints.Add(new LogPoint(GenerateArguement(0), GenerateValue(0)));
+            if (DataPoints.Count == MaxPointCount) { timer.Stop(); }
+        }
+        private DateTime GenerateArguement(int x)
+        {
+            DataSet ds = new DataSet();
+            string query = @"SELECT CURRENTDATE 시간
+                                 FROM LOG GROUP BY CURRENTDATE ORDER BY CURRENTDATE ASC";
+
+            OracleDBManager.Instance.ExecuteDsQuery(ds, query);
+            DateTime a = Convert.ToDateTime(ds.Tables[0].Rows[x]["시간"].ToString());
+            return a;
+        }
+
+        private int GenerateValue(int y)
+        {
+            DataSet ds = new DataSet();
+            string query = @"SELECT COUNT(*) 갯수 
+                                 FROM LOG GROUP BY CURRENTDATE ORDER BY CURRENTDATE ASC";
+
+            OracleDBManager.Instance.ExecuteDsQuery(ds, query);
+            int b = int.Parse(ds.Tables[0].Rows[y]["갯수"].ToString());
+            return b;
+        }
+    }
+    public class LogPoint
+    {
+        public DateTime Argument { get; set; }
+        public int Value { get; set; }
+        public LogPoint(DateTime argument, int value)
+        {
+            this.Argument = argument;
+            this.Value = value;
+        }
+    }
+}
