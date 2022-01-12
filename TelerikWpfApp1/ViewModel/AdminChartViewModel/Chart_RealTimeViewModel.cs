bafcd60996161a4cb58ca883e33bb4758efc3b56
Eol Kim <32378990+KimEul@users.noreply.github.com>
2022-01-12 21:47:01 +0900

    코드 정리

--- TelerikWpfApp1/ViewModel/AdminChartViewModel/Chart_RealTimeViewModel.cs
+++ TelerikWpfApp1/ViewModel/AdminChartViewModel/Chart_RealTimeViewModel.cs
@@ -26,40 +26,24 @@ namespace Meiday.ViewModel.AdminChartViewModel
         }
         private void Timer_Tick(object sender, System.EventArgs e)
         {
-            string today = DateTime.Now.ToString("yyyy-MM-dd");
             DateTime now = DateTime.Now.AddMinutes(-52);
-            string length = now.ToString("HH-mm-ss");
-            string   tx_now = (now.ToString()).Substring(14, 4);
-            string aaa = DateTime.Now.ToString();
-            string ampm = aaa.Substring(11, 2);
             count++;
-            
             this.DataPoints.Add(new Chart_RealTime(now, GenerateValue()));
             if (DataPoints.Count == MaxPointCount) { timer.Stop(); }
-
-
-
         }
 
         public double GenerateValue()
         {
-            
-            //string hour    = b.Substring(0, 2);
-            //string minute  = b.Substring(3, 2);
-            //string second  = b.Substring(6, 2);
-            //string query_b = hour + ":" + minute + ":" + second;
-
                 DataSet ds = new DataSet();
-               // string query = @"SELECT TO_CHAR(CURRENTDATE, 'HH24:MI:SS') 시간,COUNT(*) 갯수
-               //                FROM LOG WHERE TO_CHAR(CURRENTDATE, 'HH24:MI')>= " + "'" + query_b + "'" + "AND TO_CHAR(CURRENTDATE,'YYYY-MM-DD')=" + "'" +c + "'" + "GROUP BY TO_CHAR(CURRENTDATE, 'HH24:MI:SS')ORDER BY TO_CHAR(CURRENTDATE, 'HH24:MI:SS') ASC";
-               string query = @"select to_char(chk_date,'HH24:mi:ss') 시간, count(chk_date) 갯수
-                                from(
-                                SELECT DISTINCT SYSDATE - (LEVEL / 24 / 60 / 60 * 1 - 1) AS CHK_DATE
-                                FROM DUAL
-                                CONNECT BY LEVEL <= ((24 * 60 * 60) / 1)
-                                ORDER BY CHK_DATE
-                                ) A LEFT OUTER JOIN LOG L ON TO_CHAR(A.CHK_DATE,'HH24:mi:ss') = TO_CHAR(L.CURRENTDATE, 'HH24:mi:ss')
-                                  WHERE TO_CHAR(A.CHK_DATE,'HH24:mi') >= '16:39' group by CHK_DATE ORDER BY CHK_DATE";
+                string query = @"select to_char(chk_date,'HH24:mi:ss') 시간, count(chk_date) 갯수
+                                   from(
+                                        SELECT DISTINCT SYSDATE - (LEVEL / 24 / 60 / 60 * 1 - 1) AS CHK_DATE
+                                        FROM DUAL
+                                        CONNECT BY LEVEL <= ((24 * 60 * 60) / 1)
+                                        ORDER BY CHK_DATE
+                                       ) A LEFT OUTER JOIN LOG L 
+                                                       ON TO_CHAR(A.CHK_DATE,'HH24:mi:ss') = TO_CHAR(L.CURRENTDATE, 'HH24:mi:ss')
+                                  WHERE TO_CHAR(A.CHK_DATE,'HH24:mi') >= '16:40' group by CHK_DATE ORDER BY CHK_DATE";
 
             OracleDBManager.Instance.ExecuteDsQuery(ds, query);
             if (ds.Tables[0].Rows.Count == 0) { timer.Stop(); }
